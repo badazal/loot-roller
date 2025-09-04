fc11898541defb78df7e725b333f32313465261d
Olga Skowronek <olga@Olgas-MacBook-Pro.local>
2025-09-03 18:31:00 -0600

    changing lav luck message

--- roller4.js
+++ roller4.js
@@ -172,7 +172,7 @@ document.getElementById("roll-button").addEventListener("click", () => {
     let finalChance = e.chance || 0;
     if (e.category === "companion" && companionBonus > 0) {
       finalChance += companionBonus;
-      e.note = `(Lavinia's Luck applied: ${e.chance.toFixed(2)} → ${finalChance.toFixed(2)})`;
+      e.note = `(Lavinia's Luck applied)`;
     }
     e.finalChance = finalChance;
     e.status = rollChance(finalChance) ? "on" : "off";
